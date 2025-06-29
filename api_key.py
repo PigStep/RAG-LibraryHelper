073f8ecebdfedc286fbd7e7a6151534a570b14a1
PigStep <91902697+PigStep@users.noreply.github.com>
2025-06-29 18:17:58 +0300

    gmail API connection

--- api_key.py
+++ api_key.py
@@ -3,7 +3,10 @@ import os
 
 load_dotenv() # Get enviromental variables (API key)
 API_KEY = os.getenv("API_KEY")
-DB_KEY = os.getenv("DB_KEY")
+
+DB_KEY= os.getenv("DB_KEY")
+DB_URL= os.getenv("DB_URL")
 
 print(f"LLM API key '{API_KEY}' have been setted")
 print(f"DB API key '{DB_KEY}' have been setted")
+print(f"DB URL '{DB_KEY}' have been setted")
